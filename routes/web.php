526d89200f51541c2978449fd94f2f50a2c0686b
Tokidaze <hosseinalali@chasacademy.se>
2017-12-12 10:13:06 +0100

    create new header.blade and new header.scss files. Also installed npm with bulma

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/header', function () {
+    return view('layouts.header');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
