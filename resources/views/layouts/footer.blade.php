ef1b8a9a4d3c7dc06dcd23469acdb46a816aca36
Laila H <31241889+Laloo89@users.noreply.github.com>
2017-12-15 14:46:24 +0100

    change background

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,21 @@
+
+
+    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
+    
+<footer class="footer">
+
+
+<div class="columns">
+  <div class="column has-text-left">
+      <p>Copy by Berserkers</p> 
+  <p><i class="fa fa-map-marker" aria-hidden="true">Location</i><p> 
+      
+  <i class="fa fa-twitter" aria-hidden="true"></i>
+  <i class="fa fa-facebook-official" aria-hidden="true"></i>
+
+<i class="fa fa-github" aria-hidden="true"></i>
+  </div>
+</div>
+</footer>
