8f3e9ed053ed9a147f2288d0090b6497a15d7e3f
Laila H <31241889+Laloo89@users.noreply.github.com>
2017-12-18 20:51:09 +0100

    update to match new design

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -5,21 +5,34 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
     
 <footer class="footer">
-
-
-<div class="columns">
-  <div class="column has-text-left">
-      <p>Copy by Berserkers</p> 
- <p> <i class="fa fa-map-marker" aria-hidden="true"> Location</i></p>
- <div class="icon">     
- <nav class="level is-mobile">
-  <div class="level-item has-text-centered">
-  <i class="fa fa-twitter" aria-hidden="true"></i>
-  <i class="fa fa-facebook-official" aria-hidden="true"></i>
-  <i class="fa fa-github" aria-hidden="true"></i>
-   </div>
-  </nav>
-   </div>
-  </div>
+        <ul class="flex-box1">
+          <li></li>
+        </ul>
+        <ul class="flex-box2">
+          <li></li>
+        </ul>
+        <br>
+        <ul class="flex-box3">
+          <li></li>
+        </ul>
+        <ul class="flex-box4">
+          <li></li>
+        </ul>
+        <br>
+        <ul class="flex-box5">
+            <div class="columns">
+            <div class="column has-text-left">
+            <br><p>Back to Top!</p> 
+            <div class="icon">     
+                <nav class="level is-mobile">
+                <div class="level-item has-text-centered">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                <i class="fa fa-github" aria-hidden="true"></i>
+                </div>
+            </nav>
+        </div>
+    </div>
  </div>
+</ul>               
 </footer>
