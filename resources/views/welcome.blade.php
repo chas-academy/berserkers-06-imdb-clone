efe541715aa60eec7d05a324df756173b8a8b5f8
Laila H <31241889+Laloo89@users.noreply.github.com>
2017-12-18 16:18:37 +0100

    update page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,54 +19,43 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
       <body>
         <main>
-      
             <div>
-            <div class="container">
-  <div class="notification">
-  <h1>Top 5 movies this month</h1>
-  <figure class="image is-3by2">
-  <img src="https://a.ltrbxd.com/resized/sm/upload/z6/e0/vw/uy/pulp-fiction-65-1200-1200-675-675-crop-000000.jpg?k=0e0befe63c">
-</figure>
-  </div>
-  <article>
-           <div>
-           <button class="button level-item">Add</button>
-         </div>
+                <div class="container">
+                    <div class="notification">
+                        <figure class="image is-4by3">
+                            <img src="http://www.martincuff.com/wp-content/uploads/2016/06/Penny-Dreadful.jpg">
+                        </figure>
+                    </div> 
+                </div>
+            </div>
+            <div>
+                <h2>Community</h2>
+                
+                <article>
+                    <div class="card-content column mobile-movie-elements" >
+                        <h3>Daily Pics</h3>
+                        <h4>Community</h4>
+                       <p>Admins daily selection of reviews and charts <br>- written by our users</p>
+                    </div>
+                </article>
+                <article>
                         <div>
-                            <h2>Movie title</h2>
+                            <button class="button level-item">Add</button>
+                        </div>
+                        <div>
+                            <p>Movie title<p>
                             <p>PG-rating | length | genre </p>
                             <p>movie plot<p>
                         </div>
-                        <div class="column mobile-movie-elements">
-                            <h2>rating</h2>
-                        </div>
-                    </div>
-                </article>
-            </div>
-            <nav class="breadcrumb is-centered" aria-label="breadcrumbs">
-         <ul>
-            <li><a href="#">Movies</a></li>
-            <li><a href="#">Series</a></li>
-            <li><a href="#">Upcoming Movies</a></li>
-            <li><a href="#">Recommended Movies</a></li>
-            <li class="is-active"><a href="#" aria-current="page">Berzerkers</a></li>
-      </ul>
-   </nav>
-            <div>
-                <h2> Playlist recommended by site admin/users<h2>
-                <article>
-                    <div>
-                        <img src="">
-                        <button><button>
-                    </div>
-                    <div class="card-content column mobile-movie-elements" >
-                        <h3>Playlist title</h3>
-                        <i>Created by<i>
-                        <p>Genre<p>
-                        <p>Average rating<p>
-                        <p>Description<p>
-                    </div>
-                </article>
+                    </article>
+                <nav class="breadcrumb is-centered" aria-label="breadcrumbs">
+                <ul>
+                    <li><a href="#">Movies</a></li>
+                    <li><a href="#">Series</a></li>
+                    <li><a href="#">Upcoming Movies</a></li>
+                    <li><a href="#">Recommended Movies</a></li>
+                </ul>
+            </nav>
             </div>
             </div>
         </main>
