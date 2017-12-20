4c20239505c960e19bd14fee5fbcd0429c5a22e4
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2017-12-20 16:25:21 +0100

    create markup and basic layout for item-meta-info
    	* add som basic styling
    	* add markup for tables
    	* add new varibles for scss

--- resources/views/layouts/item_meta_info.blade.php
+++ resources/views/layouts/item_meta_info.blade.php
@@ -12,13 +12,50 @@
       <link href="{{asset('css/item_meta_info.css')}}" rel="stylesheet">
    </head>
   </body>
-    <div>
-      <h1>STAR WARS: THE LAST JEDI</h1>
-      <ul>
+    <div class="item-meta-info">
+      <h1 id="hero-header">STAR WARS: THE LAST JEDI</h1>
+      <ul class="title-genres">
         <li>Action</li>
-        <li>Adveture</li>
+        <li>Adventure</li>
         <li>Fantasy</li>
-      <ul>
+      </ul>
+      <div class="meta-info-group">
+        <div class="seasons-table">
+          <table>
+            <thead>
+              <tr>
+                <th span="2">Seasons</th>
+                <th span="2">Number of Episodes</th>
+                <th span="2">Year</th>  
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td span="2"><a>1</a></td>
+                <td span="2">20</td>
+                <td span="2">1997</td>
+              </tr>
+              <tr>
+                <td span="2"><a>2</a></td>
+                <td span="2">21</td>
+                <td span="2">1998</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="facts-table">
+          <table>     
+              <tr>
+                <th span="2">Director</th>
+                <td span="2"><a>Rian Johnson</a></td>
+              </tr>
+              <tr>
+                <th span="2">Writer</th>
+                <td span="2"><a>Rian Johnson</a></td>
+              </tr>
+          </table>
+        </div>
+      </div>
     </div>
   </body>
 </html>
