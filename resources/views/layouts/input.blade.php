25fc027632e46ed9da552dafa40cadabe1c847ec
souf87 <sophie.granloef@gmail.com>
2017-12-20 14:11:32 +0100

    added article input for comments

--- resources/views/layouts/input.blade.php
+++ resources/views/layouts/input.blade.php
@@ -1,5 +1,13 @@
-<div class="field">
-<div class="control">
-<input class="input is-rounded" type="text" placeholder="Rounded input">
-</div> 
-</div> 
+<article class="media">
+  <figure class="media-left">
+    <p class="image is-64x64">
+      <img src="https://bulma.io/images/placeholders/128x128.png">
+    </p>
+  </figure>
+  <div class="media-content">
+    <div class="field">
+      <p class="control">
+        <textarea class="textarea" placeholder="Add a comment..."></textarea>
+      </p>
+    </div>
+</article> 
