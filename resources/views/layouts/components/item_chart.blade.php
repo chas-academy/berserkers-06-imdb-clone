452360a2bf00e41b9d5d9866380f69ae938213e4
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2017-12-27 01:42:20 +0100

    create l

--- resources/views/layouts/components/item_chart.blade.php
+++ resources/views/layouts/components/item_chart.blade.php
@@ -12,121 +12,120 @@
       <link href="{{asset('css/item_chart.css')}}" rel="stylesheet">
    </head>
    <body>
-   <main>
-    <section>
-      <ul class="flex-container">
-        <article>
-          <div class="card-content column mobile-movie-elements">
-            <li class="flex-item5"><h2>Charts</h2>
-            <h3>Trending Now</h3></li>
+    <main>
+      <article class="chart-section">
+        <section class="flex-container">
+          <img src="{{asset('images/logo.svg')}}">
+          <div>
+            <h2>Charts</h2>
+            <h3 class="light" >Trending Now</h3>
           </div>
-        </article>
-      </ul>
-      <div class="chart">
-        <div class="min-item">
-          <div  class="item-img">
-            <img src="https://image.tmdb.org/t/p/w154/iO9aZzrfmMvm3IqkFiQyuuUMLh2.jpg">
-          </div>
-          <div class="card">
-            <div>
-              <div>
-                <h4>Office Space</h4>
-                <h4>10.0</h4>
-              </div>
-              <div style="visibility: hidden">
-                <h4 class="light">Season 1</h4>
-              </div>
+        </section>
+        <section class="chart">
+          <section class="min-item">
+            <div  class="item-img">
+              <img id="img1">
             </div>
-            <div>
+            <section class="card">
+              <section>
+                <div>
+                  <h4>Office Space</h4>
+                  <h4>10.0</h4>
+                </div>
+                <div style="visibility: hidden">
+                  <h4 class="light">Season 1</h4>
+                </div>
+              </section>
+              <section>
+                <div>
+                  <h4>(1999)</h4>
+                </div>
+                <div>
+                  <ul>
+                    <li>Comedy</li>
+                  <ul>
+                </div>
+              </section>
+            </section>
+          </section>
+          <div class="min-item">
+            <div  class="item-img">
+              <img id="img2">
+            </div>
+            <div class="card">
               <div>
-                <h4>(1999)</h4>
+                <div>
+                  <h4>Inception</h4>
+                  <h4>10.0</h4>
+                </div>
+                <div style="visibility: hidden">
+                  <h4 class="light">Season 1</h4>
+                </div>
               </div>
               <div>
+                <div>
+                  <h4>(1999)</h4>
+                </div>
                 <ul>
-                  <li>Comedy</li>
+                  <li>Action</li>
+                  <li>Adventure</li>
+                  <li>Sci-Fi</li>
+                <ul>
+              </div>
+            </div>
+          </div>
+          <div class="min-item">
+            <div  class="item-img">
+              <img id="img3">
+            </div>
+            <div class="card">
+              <div>
+                <div>
+                  <h4>Fight Club</h4>
+                  <h4>10.0</h4>
+                </div>
+                <div style="visibility: hidden">
+                  <h4 class="light">Season 1</h4>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <h4>(2010)</h4>
+                </div>
+                <ul>
+                  <li>Drama</li>
+                <ul>
+              </div>
+            </div>
+          </div>
+          <div class="min-item">
+            <div  class="item-img">
+              <img id="img4">
+            </div>
+            <div class="card">
+              <div>
+                <div>
+                  <h4>Twin Peaks</h4>
+                  <h4>10.0</h4>
+                </div>
+                <div>
+                  <h4 class="light">Season 3</h4>
+                </div>
+              </div>
+              <div>
+                <div>
+                  <h4>(2017)</h4>
+                </div>
+                <ul>
+                  <li>Crime</li>
+                  <li>Drama</li>
+                  <li>Mystery</li>
                 <ul>
               </div>
             </div>
           </div>
         </div>
-        <div class="min-item">
-          <div  class="item-img">
-            <img src="https://image.tmdb.org/t/p/w154/qmDpIHrmpJINaRKAfWQfftjCdyi.jpg">
-          </div>
-          <div class="card">
-            <div>
-              <div>
-                <h4>Inception</h4>
-                <h4>10.0</h4>
-              </div>
-              <div style="visibility: hidden">
-                <h4 class="light">Season 1</h4>
-              </div>
-            </div>
-            <div>
-              <div>
-                <h4>(1999)</h4>
-              </div>
-              <ul>
-                <li>Action</li>
-                <li>Adventure</li>
-                <li>Sci-Fi</li>
-              <ul>
-            </div>
-          </div>
-        </div>
-        <div class="min-item">
-          <div  class="item-img">
-            <img src="https://image.tmdb.org/t/p/w154/adw6Lq9FiC9zjYEpOqfq03ituwp.jpg">
-          </div>
-          <div class="card">
-            <div>
-              <div>
-                <h4>Fight Club</h4>
-                <h4>10.0</h4>
-              </div>
-              <div style="visibility: hidden">
-                <h4 class="light">Season 1</h4>
-              </div>
-            </div>
-            <div>
-              <div>
-                <h4>(2010)</h4>
-              </div>
-              <ul>
-                <li>Drama</li>
-              <ul>
-            </div>
-          </div>
-        </div>
-        <div class="min-item">
-          <div  class="item-img">
-            <img src="https://image.tmdb.org/t/p/w154/6W6zWBAay6XVBgJsOu6c2mlFZ8l.jpg">
-          </div>
-          <div class="card">
-            <div>
-              <div>
-                <h4>Twin Peaks</h4>
-                <h4>10.0</h4>
-              </div>
-              <div>
-                <h4 class="light">Season 3</h4>
-              </div>
-            </div>
-            <div>
-              <div>
-                <h4>(2017)</h4>
-              </div>
-              <ul>
-                <li>Crime</li>
-                <li>Drama</li>
-                <li>Mystery</li>
-              <ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  </main>
+      </article>
+    </main>
   </body>
 </html>
