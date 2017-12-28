3bc3286a3fadde3940cfec1e3297aeaa89153935
Tokidaze <hosseinalali@chasacademy.se>
2017-12-28 16:02:53 +0100

    Add social links with logos

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -21,11 +21,28 @@
                 <div class="column is-2" id="foot-1"></div>
                 <div class="column is-2 is-offset-8" id="foot-2"></div>
                 <div class="column is-3" id="foot-3">
-                    <a href="#" id="to-top">Back to the top!</a>
+                    <a href="#" id="to-top">Back to the top! (placeholder)</a>
                 </div>
                 <div class="column is-3 is-offset-6" id="foot-4"></div>
                 <div class="column is-12" id="foot-5">
-                    <p id="social-p">Stay in touch :)</p>
+                    <p id="social-p">Stay in touch :) (placeholder)</p>
+                    <div class="logo-container">
+                        <a class="social" href="#">
+                            <i class="fa fa-2x fa-facebook-official" aria-hidden="true"></i>
+                        </a>
+                        <a class="social" href="#">
+                            <i class="fa fa-2x fa-twitter" aria-hidden="true"></i>
+                        </a>
+                        <a class="social" href="#">
+                            <i class="fa fa-2x fa-google-plus-official" aria-hidden="true"></i>
+                        </a>
+                        <a class="social" href="#">
+                            <i class="fa fa-2x fa-instagram" aria-hidden="true"></i>
+                        </a>
+                        <a class="social" href="#">
+                            <i class="fa fa-2x fa-github" aria-hidden="true"></i>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
