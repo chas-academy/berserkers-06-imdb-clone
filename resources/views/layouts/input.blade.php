6e949a865c1e37ef2c9243450688e1daf557801c
souf87 <sophie.granloef@gmail.com>
2017-12-29 01:36:40 +0100

    added markup for input.blade.php

--- resources/views/layouts/input.blade.php
+++ resources/views/layouts/input.blade.php
@@ -1,19 +1,25 @@
-
-    <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
- 
-
-<article class="media">
-  <figure class="media-left">
-    <p class="image is-64x64">
-      <img src="https://bulma.io/images/placeholders/128x128.png">
-    </p>
-  </figure>
-  <div class="media-content">
-    <div class="field">
-      <p class="control">
-        <textarea class="textarea" placeholder="Add a comment..."></textarea>
-      </p>
-    </div>
-</article> 
+<html lang="{{ app()->getLocale() }}">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- CSRF Token -->
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+      --<title>{{ config('app.name', 'Laravel') }}</title>
+      <!-- Styles -->
+      <link href="{{asset('css/input.css')}}" rel="stylesheet">
+   </head>
+  </body>
+  <div class="field">
+  <div class="control">
+    <input class="input is-small" type="text" placeholder="Small input">
+  </div>
+<!--input field-->
+<div class="control">
+    <textarea class="textarea" type="text" placeholder="Normal textarea"></textarea>
+  </div>
+</div>
+  </body>
+</html>
