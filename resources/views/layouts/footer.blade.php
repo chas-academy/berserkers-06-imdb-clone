a01bec96c2ae700cd6c94a2dc3c861d2c751b3f0
Tokidaze <hosseinalali@chasacademy.se>
2017-12-29 13:51:59 +0100

    Add mobile footer, Add icons for mobile footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -16,6 +16,28 @@
 
 <body>
     <footer>
+        <div class ="is-hidden-desktop">
+            <div class="column is-12" id="mobile-column">
+                <p id="footer-text">Stay in touch! :) (placeholder)</p>
+                <a class="social" href="#">
+                    <i class="fa fa-2x fa-facebook-official" aria-hidden="true"></i>
+                </a>
+                <a class="social" href="#">
+                    <i class="fa fa-2x fa-twitter" aria-hidden="true"></i>
+                </a>
+                <a class="social" href="#">
+                    <i class="fa fa-2x fa-google-plus-official" aria-hidden="true"></i>
+                </a>
+                <a class="social" href="#">
+                    <i class="fa fa-2x fa-instagram" aria-hidden="true"></i>
+                </a>
+                <a class="social" href="#">
+                    <i class="fa fa-2x fa-github" aria-hidden="true"></i>
+                </a>
+            </div>
+        </div>
+
+        <!-- Desktop version -->
         <div class="is-hidden-mobile">
             <div class="columns is-multiline" id="column-container">
                 <div class="column is-2" id="foot-1"></div>
