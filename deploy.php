6571f4030ac81a73ccdb0aa0c345ef599001d4cf
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2017-12-30 04:26:33 +0100

    aplying temp solution, building localy then pushing to branch....

--- deploy.php
+++ deploy.php
@@ -68,7 +68,7 @@ after('deploy:failed', 'deploy:unlock');
 
 //after('deploy:update_code', 'npm:install');
 after('deploy:symlink', 'php-fpm:restart');
-after('deploy:symlink', 'npm:build');
+//after('deploy:symlink', 'npm:build');
 after('deploy:symlink', 'artisan:db:seed');
 // Migrate database before symlink new release.
 
