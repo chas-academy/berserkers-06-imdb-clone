a484531754996132000fb5adc328bafc405e8478
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-03 00:37:40 +0100

    move carousel.blade to components

---
+++ resources/views/layouts/components/carousel.blade.php
@@ -0,0 +1,28 @@
+<div class='carousel'>
+  <div class='carousel-container'>
+    <div class='carousel-content carousel-animate carousel-animate-slide'>
+      <div class='carousel-item'>
+        <img class="is-background" src="https://wikiki.github.io/images/merry-christmas.jpg" alt="" width="640" height="310" />
+        <div class="title">Merry Christmas</div>
+      </div>
+      <div class='carousel-item'>
+        <img class="is-background" src="https://wikiki.github.io/images/singer.jpg" alt="" width="640" height="310" />
+        <div class="title">Original Gift: Offer a song with <a href="https://lasongbox.com" target="_blank">La Song Box</a></div>
+      </div>
+      <div class='carousel-item'>
+        <img class="is-background" src="https://wikiki.github.io/images/sushi.jpg" alt="" width="640" height="310" />
+        <div class="title">Sushi time</div>
+      </div>
+      <div class='carousel-item'>
+        <img class="is-background" src="https://wikiki.github.io/images/life.jpg" alt="" width="640" height="310" />
+        <div class="title">Life</div>
+      </div>
+    </div>
+    <div class="carousel-nav-left">
+      <i class="fa fa-chevron-left" aria-hidden="true"></i>
+    </div>
+    <div class="carousel-nav-right">
+      <i class="fa fa-chevron-right" aria-hidden="true"></i>
+    </div>
+  </div>
+</div>
