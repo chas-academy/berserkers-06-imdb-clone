35b6b2e22db81c4fda164ea09163019a6a7146b3
Tokidaze <hosseinalali@chasacademy.se>
2018-01-03 15:44:42 +0100

    Add form to fields

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -81,53 +81,62 @@ section('content')-->
 <div class="container" id="reg-container">
 
     <h2 id="reg-h2">We are happy to have you here! Please fill out the following lines:</h2>
-    <div class="field">
-    <p class="control has-icons-left">
-        <input class="input is-hovered" type="text" placeholder="Username">
-        <span class="icon is-small is-left">
-        <i class="fa fa-user"></i>
-        </span>
-    </p>
-    </div>
-    <div class="field">
-    <p class="control has-icons-left has-icons-right">
-        <input class="input is-hovered" type="email" placeholder="Email">
-        <span class="icon is-small is-left">
-        <i class="fa fa-envelope"></i>
-        </span>
-        <span class="icon is-small is-right">
-        <i class="fa fa-check"></i>
-        </span>
-    </p>
-    </div>
-    <div class="field">
-    <p class="control has-icons-left">
-        <input class="input is-hovered" type="password" placeholder="Password">
-        <span class="icon is-small is-left">
-        <i class="fa fa-lock"></i>
-        </span>
-    </p>
-    </div>
-    <div class="field">
-    <p class="control has-icons-left">
-        <input class="input is-hovered" type="password" placeholder="Confirm Password">
-        <span class="icon is-small is-left">
-        <i class="fa fa-lock"></i>
-        </span>
-    </p>
-    </div>
-    <div class="field is-grouped is-grouped-centered">
-    <p class="control">
-        <a class="button is-primary">
-        Submit
-        </a>
-    </p>
-    <p class="control">
-        <a class="button is-light">
-        Cancel
-        </a>
-    </p>
-    </div>
+
+    <!-- Form -->
+
+    <form method="POST" action="{{ route('register') }}">
+        {{ csrf_field() }}
+        <div class="field">
+        <p class="control has-icons-left">
+            <input class="input is-hovered" type="text" placeholder="Username">
+            <span class="icon is-small is-left">
+            <i class="fa fa-user"></i>
+            </span>
+        </p>
+        </div>
+        <div class="field">
+        <p class="control has-icons-left has-icons-right">
+            <input class="input is-hovered" type="email" placeholder="Email">
+            <span class="icon is-small is-left">
+            <i class="fa fa-envelope"></i>
+            </span>
+            <span class="icon is-small is-right">
+            <i class="fa fa-check"></i>
+            </span>
+        </p>
+        </div>
+        <div class="field">
+        <p class="control has-icons-left">
+            <input class="input is-hovered" type="password" placeholder="Password">
+            <span class="icon is-small is-left">
+            <i class="fa fa-lock"></i>
+            </span>
+        </p>
+        </div>
+        <div class="field">
+        <p class="control has-icons-left">
+            <input class="input is-hovered" type="password" placeholder="Confirm Password">
+            <span class="icon is-small is-left">
+            <i class="fa fa-lock"></i>
+            </span>
+        </p>
+        </div>
+
+    <!-- Buttons -->
+
+        <div class="field is-grouped is-grouped-centered">
+        <p class="control">
+            <a class="button is-primary" type="submit">
+            Submit
+            </a>
+        </p>
+        <p class="control">
+            <a class="button is-light" type="submit" href="/">
+            Cancel
+            </a>
+        </p>
+        </div>
+    </form>
 </div>
 
 @include('layouts.footer')
