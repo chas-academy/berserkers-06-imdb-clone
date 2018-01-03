fe7fc0ebed1015676338785c7f6845d81ce34cb6
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-03 01:45:06 +0100

    bug fix for carousel
    	* add widow innerwidth restriction to match the att media roule
    	for when only one .min-item is displayed at a time

--- resources/assets/js/carousel.js
+++ resources/assets/js/carousel.js
@@ -4,42 +4,42 @@ let itemCounter = 0;
 let startX = 0;
 const nextItem = function(e) {
 
+  if (window.innerWidth <= 800) {
 
-  if (e.type === 'touchstart') {
+    if (e.type === 'touchstart') {
 
-    startX = e.touches[0].clientX;
+      startX = e.touches[0].clientX;
 
-  } else if (e.type === 'touchend') {
+    } else if (e.type === 'touchend') {
 
-    let endX = e.changedTouches[0].clientX;
-    
-    if (startX > endX) {
+      let endX = e.changedTouches[0].clientX;
+      
+      if (startX > endX) {
 
-      itemCounter++;
+        itemCounter++;
 
-    } else if (startX < endX) {
+      } else if (startX < endX) {
 
-      itemCounter--;
+        itemCounter--;
+      }
+
+      if (itemCounter > minItems.length-1) {
+
+        itemCounter = 0;
+
+      } else if (itemCounter < 0) {
+
+        itemCounter = minItems.length-1;
+      }
+
+      this.style.display = "none";
+      minItems[itemCounter].style.display = "block";
+
     }
-
-    if (itemCounter > minItems.length-1) {
-
-      itemCounter = 0;
-
-    } else if (itemCounter < 0) {
-
-      itemCounter = minItems.length-1;
-    }
-
-    this.style.display = "none";
-    minItems[itemCounter].style.display = "block";
-  
   }
 }  
-
 
 minItems.forEach(item => {
   item.addEventListener('touchstart', nextItem);
   item.addEventListener('touchend', nextItem);
 });
-
