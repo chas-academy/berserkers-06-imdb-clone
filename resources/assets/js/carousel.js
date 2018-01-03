391e945d79c93012145fa620fbc01cee9a418c7d
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-03 01:28:02 +0100

    add func to check if a right or left swipe was made

--- resources/assets/js/carousel.js
+++ resources/assets/js/carousel.js
@@ -1,38 +1,45 @@
 const minItems = document.querySelectorAll(".min-item");
 
 let itemCounter = 0;
-
+let startX = 0;
 const nextItem = function(e) {
 
-  let points = []; 
 
-  for ( let i = 0; i < e.changedTouches.length; i++ ) {
-    points.push(e.changedTouches[i].clientX);
+  if (e.type === 'touchstart') {
+
+    startX = e.touches[0].clientX;
+
+  } else if (e.type === 'touchend') {
+
+    let endX = e.changedTouches[0].clientX;
+    
+    if (startX > endX) {
+
+      itemCounter++;
+
+    } else if (startX < endX) {
+
+      itemCounter--;
+    }
+
+    if (itemCounter > minItems.length-1) {
+
+      itemCounter = 0;
+
+    } else if (itemCounter < 0) {
+
+      itemCounter = minItems.length-1;
+    }
+
+    this.style.display = "none";
+    minItems[itemCounter].style.display = "block";
+  
   }
-
-  console.log(points);
-
-  if (points[0] < points[points.length-1]) {
-    itemCounter++;
-  } else {
-    itemCounter--;
-  }
-
-  this.style.display = "none";
-
-
-  if (itemCounter > minItems.length-1) {
-    itemCounter = 0;
-  } else if (itemCounter < 0) {
-    itemCounter = minItems.length-1;
-  }
-  console.log(itemCounter);
-  minItems[itemCounter].style.display = "block";
-
 }  
 
 
 minItems.forEach(item => {
+  item.addEventListener('touchstart', nextItem);
   item.addEventListener('touchend', nextItem);
 });
 
