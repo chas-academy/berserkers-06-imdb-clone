e3ef82d9ac62179eaa55e30c584bd383eda134ed
Anna Ross <anna.ross@chasacademy.se>
2018-01-08 17:12:29 +0100

    Fix bulma config

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -14,7 +14,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <!-- Styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
     @if (!isset($routeName)) 
      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @elseif (isset($routeName))
@@ -24,7 +23,6 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-  
 </head>
 
 <body>
