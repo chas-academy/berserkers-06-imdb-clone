6606c347bb70509fdfcd262887b23b3e4299717f
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-09 18:25:24 +0100

    fix markup and layout in movie.show blade
    	* add markup from item_meta_info
    	* include header and footer
    	* add logic to loops so to not render to many actors etc and to
      	  only add if exists
    	* remove (most of) old markup

--- resources/views/titles/movies/show.blade.php
+++ resources/views/titles/movies/show.blade.php
@@ -1,79 +1,146 @@
-@extends('layouts.app')
-@section('content')
-        <a href="http://{{ $_SERVER['HTTP_HOST'] }}/titles/movies">Back to all movies</a>        
-        @foreach($title->photos as $photo)
-            @if($photo->width == 500 && $photo->photo_type == "poster")
-                <img src="{{ $photo->photo_path }}" alt="poster" width="300">
-                @break
-            @endif
-            @if($loop->last)
-                <img src="{{ $photo->photo_path }}" alt="poster" width="300">
-            @endif
-        @endforeach
-        <br>
-        @foreach($title->photos as $photo)
-            @if($photo->width == 1280 && $photo->photo_type == "backdrop")
-                <img src="{{ $photo->photo_path }}" alt="poster">
-                @break
-            @endif
-            @if($loop->last)
-                <img src="{{ $photo->photo_path }}" alt="poster">
-            @endif
-        @endforeach
-
-        <p>{{ $movie->plot_summary }}</p>
-        <br>
-        <p>Genres: 
-        @foreach($title->genres as $genre)
-            {{ $loop->first ? '' : ', ' }}
-            {{ $genre->name }}
-        @endforeach</p>
-        <p>Rating: 
-        <?php
-            $ratingSummary = 0;
-            $i = 0;
-            foreach ($title->ratings as $rating) {
-                $ratingSummary = $ratingSummary + $rating->rating;
-                $i++;
-            }
-            if ($i == 0) {
-                echo "-";
-            } else {
-                $ratingSummary = $ratingSummary / $i;
-                echo $ratingSummary;
-            }
-        ?>
-        </p>
-        <article>
-            <h3>Directors:</h3>
-        @foreach($title->directors as $director)
-            <a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $director->id }}">{{ $director->name }}</a><br>
-        @endforeach</article>
-        <article>
-            <h3>Producers:</h3>
-        @foreach($title->producers as $producer)
-            <a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $producer->id }}">{{ $producer->name }}</a><br>
-        @endforeach</article>
-        <article>
-            <h3>Screenwriters:</h3>
-        @foreach($title->screenwriters as $screenwriter)
-            <a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $screenwriter->id }}">{{ $screenwriter->name }}</a><br>
-        @endforeach</article>
-        <article>
-            <h3>Actors:</h3>
-        @foreach($title->characters as $character)
-            <table>
-            @foreach ($character->actor as $actor)
-                <tr>
-                    <td style="width: 200px"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $actor->id }}">{{ $actor->name }}</a></td>
-                    <td style="width: 30px">as</td>
-                    <td>{{ $character->character_name }}</td>
-                </tr>
+@include('layouts.header')
+        <section class="item-header">
+            <h1 id="hero-header">{{$movie->title}}</h1>
+        </section>
+        <article class="item">
+            <section class="item-meta-info">
+            <ul class="title-genres">
+                @for($i = 0; $i < 3; $i++)
+                    @if (isset($title->genres[$i]))
+                        <li>{{ $title->genres[$i]->name }}</li>
+                    @endif
+                @endfor
+            </ul>
+            <div class="meta-info-group">
+                <section class="row-flex-start">
+                <h2><span>short</span><span>Facts</span></h2>
+                </section>
+                <section class="facts-table">
+                <table>     
+                    @foreach($title->directors as $key => $director)
+                        @if ($key === 0 && $key === count($title->directors)-1)
+                            <tr class="row-padding-botom">
+                                <th span="2">Director</th>
+                                <td class="link"span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $director->id }}">{{ $director->name }}</a></td>
+                            </tr>
+                        @elseif ($key === 0)
+                            <tr>
+                                <th span="2">Directors</th>
+                                <td class="link"span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $director->id }}">{{ $director->name }}</a></td>
+                            </tr>
+                        @elseif ($key === count($title->directors)-1)
+                            <tr class="table-flex-end row-padding-botom">
+                                <td class="link"span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $director->id }}">{{ $director->name }}</a></td>
+                            </tr>
+                        @else
+                            <tr class="table-flex-end">
+                                <td class="link"span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $director->id }}">{{ $director->name }}</a></td>
+                            </tr>
+                        @endif
+                    @endforeach
+                    @for($i = 0; $i < 2; $i++)
+                        @if(isset($title->producers[$i]))
+                            @if($i === 0 && isset($title->producers[$i+1]))
+                                <tr>
+                                    <th span="2">Producers</th>
+                                    <td class="link"span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $title->producers[$i]->id }}">{{ $title->producers[$i]->name }}</a></td>
+                                </tr>
+                            @elseif ($i === 0 && !isset($title->producers[$i+1]))
+                                <tr class="row-padding-botom">
+                                    <th span="2">Producer</th>
+                                    <td class="link"span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $title->producers[$i]->id }}">{{ $title->producers[$i]->name }}</a></td>
+                                </tr>
+                            @else
+                                <tr class="table-flex-end row-padding-botom">
+                                    <td class="link"span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $title->producers[$i]->id }}">{{ $title->producers[$i]->name }}</a></td>
+                                </tr>
+                            @endif
+                        @endif
+                    @endfor
+                    @foreach($title->screenwriters as $key => $screenwriter)
+                        @if ($key === 0 && $key === count($title->screenwriters)-1)
+                            <tr class="row-padding-botom">
+                                <th span="2">Writer</th>
+                                <td class="link" span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $screenwriter->id }}">{{ $screenwriter->name }}</a></td>
+                            </tr>
+                        @elseif ($key === 0)
+                            <tr>
+                                <th span="2">Writers</th>
+                                <td class="link" span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $screenwriter->id }}">{{ $screenwriter->name }}</a></td>
+                            </tr>
+                        @elseif ($key === count($title->screenwriters)-1)
+                            <tr class="table-flex-end row-padding-botom">
+                                    <td class="link" span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $screenwriter->id }}">{{ $screenwriter->name }}</a></td>
+                            </tr>
+                        @else
+                            <tr class="table-flex-end">
+                                    <td class="link" span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $screenwriter->id }}">{{ $screenwriter->name }}</a></td>
+                            </tr>
+                        @endif
+                    @endforeach
+                    @for($i = 0; $i < 5; $i++)
+                        @if(isset($title->actors[$i]))
+                            @if($i === 0)
+                                <tr>
+                                    <th span="2">Cast</th>
+                                    <td class="link" span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $title->actors[$i]->id }}">{{ $title->actors[$i]->name }}</a></td>
+                                </tr>
+                            @elseif ($i === 4 || !isset($title->actors[$i+1]))
+                                <tr class="table-flex-end row-padding-botom">
+                                    <td class="link" span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $title->actors[$i]->id }}">{{ $title->actors[$i]->name }}</a></td>
+                                </tr>
+                                <tr class="table-flex-end alt-color">
+                                        <td class="link" span="2"><a>VIEW FULL CAST</a></td>
+                                </tr>
+                            @else 
+                                <tr class="table-flex-end">
+                                    <td class="link" span="2"><a href="http://{{ $_SERVER['HTTP_HOST'] }}/people/{{ $title->actors[$i]->id }}">{{ $title->actors[$i]->name }}</a></td>
+                                </tr>
+                            @endif
+                        @endif
+                    @endfor
+                </table>
+                <section class="plot-sumary">
+                    <h3>PLOT SUMMARY</h3>
+                    <div class="card">
+                    <p>{{ $movie->plot_summary }}</p>
+                    </div>
+                </section>
+                </section>
+            </div>
+            </section>
+            <section class="item-img card">
+            @foreach($title->photos as $photo)
+                @if($photo->width == 1280 && $photo->photo_type == "backdrop")
+                    <img id="title-img" src="{{ $photo->photo_path }}" alt="poster">
+                    @break
+                @endif
+                @if($loop->last)
+                    <img id="title-img" src="{{ $photo->photo_path }}" alt="poster">
+                @endif
             @endforeach
-            </table>
-        @endforeach</article>
+            </section>
+        </article>
+        <a href="http://{{ $_SERVER['HTTP_HOST'] }}/titles/movies">Back to all movies</a>        
         
-        @if(Auth::check())
-            <a href="http://{{ $_SERVER['HTTP_HOST'] }}/reviews/create">Create a review</a>
-        @endif
-@endsection
+            <p>Rating: 
+            <?php
+                $ratingSummary = 0;
+                $i = 0;
+                foreach ($title->ratings as $rating) {
+                    $ratingSummary = $ratingSummary + $rating->rating;
+                    $i++;
+                }
+                if ($i == 0) {
+                    echo "-";
+                } else {
+                    $ratingSummary = $ratingSummary / $i;
+                    echo $ratingSummary;
+                }
+            ?>
+            </p>
+            
+            @if(Auth::check())
+                <a href="http://{{ $_SERVER['HTTP_HOST'] }}/reviews/create">Create a review</a>
+            @endif
+@include('layouts.footer')
