a9522a5de9d3dbcb815001cdbb01c841a8e4f7de
AnnaRoss <31239061+AnnaRoss@users.noreply.github.com>
2018-01-09 14:19:38 +0100

    Update catalog.blade.php

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -1,7 +1,3 @@
 @include('layouts.header')
-<<<<<<< HEAD
-=======
-<p>hej</p>
->>>>>>> db91c1563c3e1c0997a6cef9681c4985f8481f5b
 @include('layouts.components.filter_panel')
 @include('layouts.footer')
