f8ae4e67082cf21179bbbdc83a2f7c855c96b6fb
Tokidaze <hosseinalali@chasacademy.se>
2018-01-10 13:54:40 +0100

    add changes to modal for login

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,35 +1,29 @@
 @php 
-
     use Illuminate\Support\Facades\Route;
-
     $routeName = Route::currentRouteName();
-    
 @endphp
-
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
    <head>
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <!-- Styles -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
-    @if (!isset($routeName)) 
-     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    @elseif ($routeName === "register" && isset($routeName))
-    <link href="{{ asset('css/' . $routeName . '.css') }}" rel="stylesheet">
-    @endif
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>{{ config('app.name', 'Laravel') }}</title>
-  
-</head>
-
-<body>
-    <header>
-        <!-- Burger logo -->
-        <div class="is-hidden-desktop">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css">
+      @if (!isset($routeName)) 
+      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+      @elseif ($routeName === "register" && isset($routeName))
+      <link href="{{ asset('css/' . $routeName . '.css') }}" rel="stylesheet">
+      @endif
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- CSRF Token -->
+      <meta name="csrf-token" content="{{ csrf_token() }}">
+      <title>{{ config('app.name', 'Laravel') }}</title>
+   </head>
+   <body>
+      <header>
+         <!-- Burger logo -->
+         <div class="is-hidden-desktop">
             <nav class="navbar is-fixed-top" id="mobile-navbar">
                <div class="navbar-brand">
                   <div class="navbar-burger burger" data-target="Options">
@@ -67,42 +61,47 @@
                </div>
             </nav>
          </div>
-        <!-- Modal -->
-        <div class="modal">
-         <div class="modal-background"></div>
-             <div class="modal-content">
-                 <header class "modal-card-head">
-                     <p class="modal-card-title">Log in or Register</p>
-                 </header>
-                 <!-- Any other Bulma elements you want -->
-                 <div class="field">
+         <!-- Modal -->
+         <div class="modal">
+            <div class="modal-background"></div>
+            <div class="modal-content">
+               @if (!Auth::user())
+               <header class "modal-card-head">
+                  <p class="modal-card-title">Log in or Register</p>
+               </header>
+               <!-- Any other Bulma elements you want -->
+               <form method ="POST" action="{{ route('login') }}" >
+                  {{ csrf_field() }}
+                  <div class="field">
                      <p class="control has-icons-left has-icons-right">
-                         <input class="input" type="email" placeholder="Email">
-                         <span class="icon is-medium is-left">
-                         <i class="fa fa-envelope"></i>
-                         </span>
-                         <span class="icon is-medium is-right">
-                         <i class="fa fa-check"></i>
-                         </span>
+                        <input class="input" type="username" placeholder="Username" name="username" value="{{ old('username') }}" required>
+                        <span class="icon is-medium is-left">
+                        <i class="fa fa-envelope"></i>
+                        </span>
+                        <span class="icon is-medium is-right">
+                        <i class="fa fa-check"></i>
+                        </span>
                      </p>
-                 </div>
-                 <div class="field">
+                  </div>
+                  <div class="field">
                      <p class="control has-icons-left">
-                         <input class="input" type="password" placeholder="Password">
-                         <span class="icon is-medium is-left">
-                         <i class="fa fa-lock"></i>
-                         </span>
+                        <input class="input" type="password" placeholder="Password" name="password" required>
+                        <span class="icon is-medium is-left">
+                        <i class="fa fa-lock"></i>
+                        </span>
                      </p>
-                 </div>
-                 <div class="field">
+                  </div>
+                  <div class="field">
                      <p class="control">
-                         <button class="button is-success">Login</button>
-                         <span class="button is-danger">Cancel</span>
-                         <a class="button is-info" id="register" href="/register">Register</a>
+                        <button type="submit" class="button is-success">Login</button>
+                        <span class="button is-danger">Cancel</span>
+                        <a href="/register"><span class="button is-info" id="register" >Register</span></a>
                      </p>
-                 </div>
-             </div>
-        </div>
+                  </div>
+               </form>
+               @endif
+            </div>
+         </div>
          <!-- Desktop -->
          <div class="is-hidden-mobile">
             <nav class="navbar is-fixed" role="navigation" aria-label="main navigation">
@@ -137,14 +136,13 @@
                               <a class="button is-primary" type="submit" href="/register">Register</a>
                            </div>
                         </div>
-                    </div>
-                </div>
-                <a class="navbar-item" id="desktop-logo" href="/">
-                    <img src="{{asset('images/LOGO.svg')}}" id="bzrk2" alt="">
-                </a>
-        </div>
-        </nav>
-        </div>
-    </header>
-    <main>
-
+                     </div>
+                  </div>
+                  <a class="navbar-item" id="desktop-logo" href="/">
+                  <img src="{{asset('images/LOGO.svg')}}" id="bzrk2" alt="">
+                  </a>
+               </div>
+            </nav>
+         </div>
+      </header>
+      <main>
