f7cadf8688b0b3bbb36c38a2e451753253aa0ab8
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-12 23:56:07 +0100

    add route for deleting a single episode

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,7 @@ Route::get('titles/series/{series_id}/seasons/{season_number}/episodes', 'Episod
 Route::get('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@show');
 Route::get('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}/edit', 'EpisodesController@edit')->name('edit');
 Route::put('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@update');
+Route::delete('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@destroy');
 
 Route::resource('people', 'PeopleController');
 Route::resource('reviews', 'ReviewsController');
