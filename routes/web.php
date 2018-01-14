bf9f764d34a283400a580e917d9bc83363662e62
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-14 17:08:30 +0100

    restrict access to update,edit,destroy routes to athuenticated users

--- routes/web.php
+++ routes/web.php
@@ -24,16 +24,27 @@ Route::get('/inputs', function () {
 Auth::routes();
 
 Route::get('/titles', 'TitlesController@index')->name('catalog');
-Route::resource('titles/movies', 'MoviesController')->name("edit","edit")->name('show','title');
-Route::resource('titles/series', 'SeriesController')->name("edit","edit")->name('show','title');
+
+Route::group(['middleware' => ['auth']], function () {
+    Route::delete('titles/series/{series}','SeriesController@destory' );
+    Route::delete('titles/movies/{movie}', 'MoviesController@destroy');
+    Route::put('titles/series/{series}','SeriesController@update' );
+    Route::put('titles/movies/{movie}', 'MoviesController@update');
+    Route::get('titles/series/{series}/edit','SeriesController@edit' )->name("edit");
+    Route::get('titles/movies/{movie}/edit', 'MoviesController@edit')->name("edit");
+    Route::delete('titles/series/{series_id}/seasons/{season_number}', 'SeasonsController@destroy');
+    Route::get('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}/edit', 'EpisodesController@edit')->name('edit');
+    Route::put('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@update');
+    Route::delete('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@destroy');
+});
+
+Route::get('titles/series/{series}','SeriesController@show' )->name("title");
+Route::get('titles/movies/{movie}', 'MoviesController@show')->name("title");
 Route::get('titles/series/{series_id}/seasons', 'SeasonsController@index');
 Route::get('titles/series/{series_id}/seasons/{season_number}', 'SeasonsController@show');
-Route::delete('titles/series/{series_id}/seasons/{season_number}', 'SeasonsController@destroy');
 Route::get('titles/series/{series_id}/seasons/{season_number}/episodes', 'EpisodesController@index');
 Route::get('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@show')->name('title');
-Route::get('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}/edit', 'EpisodesController@edit')->name('edit');
-Route::put('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@update');
-Route::delete('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@destroy');
+
 
 Route::resource('people', 'PeopleController');
 Route::resource('reviews', 'ReviewsController');
