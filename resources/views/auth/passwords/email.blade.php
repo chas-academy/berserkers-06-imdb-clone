02036290146016842f1e8a5f8c38aada6b73fda1
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-20 03:06:47 +0100

    edit prexsisting markup

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,16 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
+@include('layouts.header')
+<section class="custom-form">
+    <form class="field" method="POST" action="{{ route('password.email') }}">
+        {{ csrf_field() }}
+        <label for="email" class="label">E-Mail Address</label>
+        <div class="control has-icons-left">
+            <input type="email" class="input" name="email" value="{{ old('email') }}" required>
+            <span class="icon is-small is-left">
+                    <i class="fa fa-envelope"></i>
+            </span>
         </div>
-    </div>
-</div>
-@endsection
+        <button type="submit" class="button is-primary">Send Password Reset Link</button>
+    </form>
+</section>
+
+@include('layouts.footer')
