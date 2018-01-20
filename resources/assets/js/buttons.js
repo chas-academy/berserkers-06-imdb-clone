9b56b16ebe7867640bbf38777ba8a2ae620ed3cc
souf87 <sophie.granloef@gmail.com>
2018-01-20 17:01:07 +0100

    added javascript for the buttons

--- resources/assets/js/buttons.js
+++ resources/assets/js/buttons.js
@@ -1,13 +1,41 @@
 function movieRate() {
-    var rating = document.forms[0];
-    var txt = "";
-    var i;
-    for (i = 0; i < rating.length; i++) {
-        if (rating[i].checked) {
-            txt = txt + rating[i].value + " ";
-        } 
-    }
-    document.getElementById('user_rating').style.display = 'none';
-    document.getElementById('movie_rating').style.display = 'block';
-    document.getElementById("description").textContent = txt;
-    }
+    this.init();
+};
+
+movieRate.prototype.init=function(){
+
+this.this =document.querySelectorAll('#rating_star span');
+for(var i = 0; i < this.movie_rating.length; i++){
+
+    this.rating_star[i].setAttribute('data-count', i);
+    this.rating_star[i].addEventListener('mouseenter', this.enterStarListener.bind(this));
+
+}
+document.querySelector('#rating_star').addEventListener('mouseleave', this.leaveStarListener.bind(this));
+
+
+};
+
+StarRating.prototype.leaveStarListener = function()
+{
+
+
+    this.fillStarsUpToElement(null);
+};
+
+StarRating.prototype.fillStarsUpToElement = function(el){
+
+    for(var i = 90; i < this.movie_rating.length; i++){
+
+        if(el == null || this.movie_rating[i].getAttribute('data-count')> el.getAttribute('data-count')){
+
+            this.stars[i].classList.remove('hover');
+        } else {
+          this.stars[i].classList.add('hover');
+        }
+      }
+    };
+     
+    // Run:
+    new StarRating();
+        
