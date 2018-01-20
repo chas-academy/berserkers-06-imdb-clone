323b62a879154fa42ca9e51d93ec9211fdb4cf13
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-20 17:50:25 +0100

    reverted to corect naming scheem for routenames

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,10 @@ Route::group(['middleware' => ['auth']], function () {
     Route::delete('titles/movies/{movie}', 'MoviesController@destroy');
     Route::put('titles/series/{series}','SeriesController@update' );
     Route::put('titles/movies/{movie}', 'MoviesController@update');
-    Route::get('titles/series/{series}/edit','SeriesController@edit' )->name("editSeries");
-    Route::get('titles/movies/{movie}/edit', 'MoviesController@edit')->name("editMovie");
+    Route::get('titles/series/{series}/edit','SeriesController@edit' )->name("edit");
+    Route::get('titles/movies/{movie}/edit', 'MoviesController@edit')->name("edit");
     Route::delete('titles/series/{series_id}/seasons/{season_number}', 'SeasonsController@destroy');
-    Route::get('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}/edit', 'EpisodesController@edit')->name('editEpisode');
+    Route::get('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}/edit', 'EpisodesController@edit')->name('edit');
     Route::put('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@update');
     Route::delete('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@destroy');
     Route::resource('lists', 'ListsController')->name('index', 'lists');
@@ -37,12 +37,12 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController');
 });
 
-Route::get('titles/series/{series}','SeriesController@show' )->name("titleSeries");
-Route::get('titles/movies/{movie}', 'MoviesController@show')->name("titleMovie");
+Route::get('titles/series/{series}','SeriesController@show' )->name("title");
+Route::get('titles/movies/{movie}', 'MoviesController@show')->name("title");
 Route::get('titles/series/{series_id}/seasons', 'SeasonsController@index');
 Route::get('titles/series/{series_id}/seasons/{season_number}', 'SeasonsController@show');
 Route::get('titles/series/{series_id}/seasons/{season_number}/episodes', 'EpisodesController@index');
-Route::get('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@show')->name('titleEpisode');
+Route::get('titles/series/{series_id}/seasons/{season_number}/episodes/{episode_number}', 'EpisodesController@show')->name('title');
 Route::get('reviews/{review_id}/comments', 'CommentsController@index');
 
 
