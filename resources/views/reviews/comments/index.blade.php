bf5e0a8f83143c6e5885acc9ffbe8c1f15b064ea
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-20 19:47:30 +0100

    add basic styling to reviews edit views

--- resources/views/reviews/comments/index.blade.php
+++ resources/views/reviews/comments/index.blade.php
@@ -1,50 +1,56 @@
 @include('layouts.header')
+    <article>
     @foreach($comments as $comment)
         <div class="user-comment">
             <p class="review-date">{{ $comment->created_at }} &nbsp;</p>
             <p class="review-user">|&nbsp;&nbsp;by user: {{ $comment->user->username }}</p>
             <p class="comment-content">{{ $comment->body }}</p>
         </div>
-        <section>
-            <form method="POST" action="{{route('comments.update', [$comment->id])}}">
+        <section class="field" >
+            <form class="control" method="POST" action="{{route('comments.update', [$comment->id])}}">
             {{ csrf_field() }}
             {{ method_field('PUT') }}
-            <h3>Comment</h3>
-            <input name="body"value="{{ $comment->body }}">
-            <button type="submit">Submit</button>  
+            <h3 class="label">Comment</h3>
+            <input class="input" name="body"value="{{ $comment->body }}">
+            <button class="button is-primary" type="submit">Submit</button>  
             </form>
         </section>
         @if($comment->status == 1)
-            <section>
-                <form method="POST" action="{{route('comments.update', [$comment->id])}}">
+            <section class="field">
+                <form class="control" method="POST" action="{{route('comments.update', [$comment->id])}}">
                 {{ csrf_field() }}
                 {{ method_field('PUT') }}
-                <h3>Status: Approved</h3>
-                <select name="status">
-                    <option value="1">Approved</option>
-                    <option value="2">Pending</option>
-                </select>
-                <button type="submit">Submit</button>   
+                <h3 class="label">Status: Approved</h3>
+                <div class="select">
+                    <select name="status">
+                        <option value="1">Approved</option>
+                        <option value="2">Pending</option>
+                    </select>
+                </div>
+                <button class="button is-success" type="submit">Submit</button>   
                 </form>
             </section>
         @else
-            <section>
-                <form method="POST" action="{{route('comments.update', [$comment->id])}}">
+            <section class="field">
+                <form class="control" method="POST" action="{{route('comments.update', [$comment->id])}}">
                 {{ csrf_field() }}
                 {{ method_field('PUT') }}
-                <h3>Status: Pending</h3>
-                <select name="status">
-                    <option value="2">Pending</option>
-                    <option value="1">Approved</option>
-                </select>
-                <button type="submit">Submit</button>   
+                <h3 class="label">Status: Pending</h3>
+                <div class="select">
+                    <select name="status">
+                        <option value="2">Pending</option>
+                        <option value="1">Approved</option>
+                    </select>
+                </div>
+                <button class="button is-success" type="submit">Submit</button>   
                 </form>
             </section>
         @endif
-        <form method="POST" action="{{ route('comments.destroy', [$comment->id]) }}">
+        <form class="control" method="POST" action="{{ route('comments.destroy', [$comment->id]) }}">
             {{ csrf_field() }}
             {{ method_field('DELETE') }}
-            <button type="submit">Delete</button>
+            <button class="button is-danger" type="submit">Delete</button>
         </form>
     @endforeach
+    </article>
 @include('layouts.footer')
--- resources/views/reviews/edit.blade.php
+++ resources/views/reviews/edit.blade.php
@@ -1,61 +1,67 @@
 @include('layouts.header')
-    <section>
-        <form method="POST" action="{{route('reviews.update', [$review->id])}}">
-        {{ csrf_field() }}
-        {{ method_field('PUT') }}
-        <h3>Title</h3>
-        <input name="title"value="{{ $review->title }}">
-        <button type="submit">Submit</button>  
-        </form>
-    </section>
-    <section>
-        <form method="POST" action="{{route('reviews.update', [$review->id])}}">
-        {{ csrf_field() }}
-        {{ method_field('PUT') }}
-        <h3>Review</h3>
-        <textarea name="body">{{ $review->body }}</textarea>
-        <button type="submit">Submit</button>   
-        </form>
-    </section>
-    <section>
-        <form method="POST" action="{{route('reviews.update', [$review->id])}}">
-        {{ csrf_field() }}
-        {{ method_field('PUT') }}
-        <h3>Title ID</h3>
-        <input name="title_id"value="{{ $review->title_id }}">
-        <button type="submit">Submit</button>   
-        </form>
-    </section>
-    @if($review->status == 1)
-        <section>
-            <form method="POST" action="{{route('reviews.update', [$review->id])}}">
+    <article>
+        <section class="field">
+            <form class="control" method="POST" action="{{route('reviews.update', [$review->id])}}">
             {{ csrf_field() }}
             {{ method_field('PUT') }}
-            <h3>Status: Approved</h3>
-            <select name="status">
-                <option value="1">Approved</option>
-                <option value="2">Pending</option>
-            </select>
-            <button type="submit">Submit</button>   
+            <h3 class="label">Title</h3>
+            <input class="input"name="title"value="{{ $review->title }}">
+            <button class="button is-primary"type="submit">Submit</button>  
             </form>
         </section>
-    @else
-        <section>
-            <form method="POST" action="{{route('reviews.update', [$review->id])}}">
+        <section class="field">
+            <form class="control" method="POST" action="{{route('reviews.update', [$review->id])}}">
             {{ csrf_field() }}
             {{ method_field('PUT') }}
-            <h3>Status: Pending</h3>
-            <select name="status">
-                <option value="2">Pending</option>
-                <option value="1">Approved</option>
-            </select>
-            <button type="submit">Submit</button>   
+            <h3 class="label">Review</h3>
+            <textarea class="textarea" name="body">{{ $review->body }}</textarea>
+            <button class="button is-primary"type="submit">Submit</button>   
             </form>
         </section>
-    @endif
-    <form method="POST" action="{{ route('reviews.destroy', [$review->id]) }}">
-        {{ csrf_field() }}
-        {{ method_field('DELETE') }}
-        <button type="submit">Delete</button>
-    </form>
+        <section class="field">
+            <form  class="control"method="POST" action="{{route('reviews.update', [$review->id])}}">
+            {{ csrf_field() }}
+            {{ method_field('PUT') }}
+            <h3 class="label">Title ID</h3>
+            <input class="input" name="title_id"value="{{ $review->title_id }}">
+            <button class="button is-primary"type="submit">Submit</button>   
+            </form>
+        </section>
+        @if($review->status == 1)
+            <section class="field">
+                <form class="control" method="POST" action="{{route('reviews.update', [$review->id])}}">
+                {{ csrf_field() }}
+                {{ method_field('PUT') }}
+                <h3 class="label">Status: Approved</h3>
+                <div class="select">
+                    <select name="status">
+                        <option value="1">Approved</option>
+                        <option value="2">Pending</option>
+                    </select>
+                </div>
+                <button class="button is-success" type="submit">Submit</button>   
+                </form>
+            </section>
+        @else
+            <section class="field">
+                <form class="control" method="POST" action="{{route('reviews.update', [$review->id])}}">
+                {{ csrf_field() }}
+                {{ method_field('PUT') }}
+                <h3 class="label">Status: Pending</h3>
+                <div class="select">
+                    <select class="select" name="status">
+                        <option value="2">Pending</option>
+                        <option value="1">Approved</option>
+                    </select>
+                </div>
+                <button class="button is-sucesss" type="submit">Change Status</button>   
+                </form>
+            </section>
+        @endif
+        <form  class="control" method="POST" action="{{ route('reviews.destroy', [$review->id]) }}">
+            {{ csrf_field() }}
+            {{ method_field('DELETE') }}
+            <button class="button is-danger"type="submit">Delete Review</button>
+        </form>
+    </article>
 @include('layouts.footer')
