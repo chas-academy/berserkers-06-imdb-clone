1c54166586c3d260b18cb637d14395360216192a
souf87 <sophie.granloef@gmail.com>
2018-01-21 23:50:10 +0100

    added javascript

--- resources/assets/js/buttons.js
+++ resources/assets/js/buttons.js
@@ -1,14 +1,36 @@
-function rateMovie(){
-var buttons = getElementsByID('#stars');getElemeentsByClassName('star1', 'star2', 'star3', 'star4', 'star5');
+//global declarations
+var rating = 3;
+var stars = null;
 
-function stars(star){
+//initial setup
+document.addEventListener('DOMContentLoaded', function(){
+  stars = document.querySelectorAll('#stars');
+  addListeners();
+  setRating(); //based on global rating variable value
+});
 
-  star.onclick = function(){
-
-    var star1 = 
-
+function addListeners(){
+  [].forEach.call(stars, function(star, index){
+    star.addEventListener('click', (function(idx){
+      console.log('adding listener', index);
+      return function(){
+        rating = idx + 1;  
+        console.log('Rating is now', rating)
+        setRating();
+      }
+    })(index));
+  });
   
 }
 
+function setRating(){
+  [].forEach.call(stars, function(star, index){
+    if(rating > index){
+      star.classList.add('rated');
+      console.log('added rated on', index );
+    }else{
+      star.classList.remove('rated');
+      console.log('removed rated on', index );
+    }
+  });
 }
-
