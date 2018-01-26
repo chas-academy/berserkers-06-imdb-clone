67d341c9bf2018231cf1f73ea2d1d85a103d55e1
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-26 16:43:05 +0100

    add basic bulma styling for edit views

--- resources/views/titles/movies/edit.blade.php
+++ resources/views/titles/movies/edit.blade.php
@@ -1,119 +1,119 @@
 @include('layouts.header')
-        <section>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Title</h3>
-            <input name="title"value="{{ $movie->title }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Plot Summary</h3>
-            <textarea name="plot_summary">{{ $movie->plot_summary }}"</textarea>
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Relesae Year</h3>
-            <input name="release_year"value="{{ $movie->release_year }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Runtime</h3>
-            <input name="runtime"value="{{ $movie->runtime }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Countries</h3>
-            <input name="countries"value="{{ $movie->countries }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>PG Rating</h3>
-            <input name="pg_rating"value="{{ $movie->pg_rating }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="PUT" action="/titles/movies/{{$title->id}}">
-            <h3>Trailer URL</h3>
-            <input name="trailer"value="{{ $movie->trailer }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Genres:</h3>
-            <textarea name="genres">{{$genres}}</textarea>
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Directors:</h3>
-            <textarea name="directors">{{$directors}}</textarea>
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Producers:</h3>
-            <textarea name="producers">{{$producers}}</textarea>
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <article>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-          <h3>Screenwriters:</h3>
-          <textarea name="screenwriters">{{$screenwriters}}</textarea>
-          <button type="submit">Submit</button>   
-        </form>
-        </article>
-        <article>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Actors:</h3>
-            <textarea name="actorsAsCharacters">{{$actorsAsCharacters}}</textarea>
-            <button type="submit">Submit</button>   
-          </form>
-        </article>
-        <section>
-          <form method="POST" action="/titles/movies/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Photos:</h3>
-            <textarea name="photos">{{$photos}}</textarea>
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-        </section>
+  <article class="form-container">
+    <section>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <h3>Title</h3>
+        <input class="input" name="title"value="{{ $movie->title }}">
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </section>
+    <section>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <h3>Plot Summary</h3>
+        <textarea class="textarea" name="plot_summary">{{ $movie->plot_summary }}"</textarea>
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </section>
+    <section>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <h3>Relesae Year</h3>
+        <input class="input" name="release_year"value="{{ $movie->release_year }}">
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </section>
+    <section>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <h3>Runtime</h3>
+        <input class="input" name="runtime"value="{{ $movie->runtime }}">
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </section>
+    <section>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <h3>Countries</h3>
+        <input class="input" name="countries"value="{{ $movie->countries }}">
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </section>
+    <section>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <h3>PG Rating</h3>
+        <input class="input" name="pg_rating"value="{{ $movie->pg_rating }}">
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </section>
+    <section>
+      <form method="PUT" action="/titles/movies/{{$title->id}}">
+        <h3>Trailer URL</h3>
+        <input class="input" name="trailer"value="{{ $movie->trailer }}">
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </section>
+    <section>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <h3>Genres:</h3>
+        <textarea class="textarea" name="genres">{{$genres}}</textarea>
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </section>
+    <section>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <h3>Directors:</h3>
+        <textarea class="textarea" name="directors">{{$directors}}</textarea>
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </section>
+    <section>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <h3>Producers:</h3>
+        <textarea class="textarea" name="producers">{{$producers}}</textarea>
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </section>
+    <article>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+      <h3>Screenwriters:</h3>
+      <textarea class="textarea" name="screenwriters">{{$screenwriters}}</textarea>
+      <button class="button is-info" type="submit">Submit</button>   
+    </form>
+    </article>
+    <article>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <h3>Actors:</h3>
+        <textarea class="textarea" name="actorsAsCharacters">{{$actorsAsCharacters}}</textarea>
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </article>
+    <section>
+      <form method="POST" action="/titles/movies/{{$title->id}}">
+        {{ csrf_field() }}
+        {{ method_field('PUT') }}
+        <h3>Photos:</h3>
+        <textarea class="textarea" name="photos">{{$photos}}</textarea>
+        <button class="button is-info" type="submit">Submit</button>   
+      </form>
+    </section>
+  <article>
 @include('layouts.footer')
--- resources/views/titles/series/edit.blade.php
+++ resources/views/titles/series/edit.blade.php
@@ -1,92 +1,92 @@
 @include('layouts.header')
-        <section>
-          <form method="POST" action="/titles/series/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Title</h3>
-            <input name="title"value="{{ $series->title }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/series/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Plot Summary</h3>
-            <textarea name="plot_summary">{{ $series->plot_summary }}"</textarea>
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/series/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Relesae Year</h3>
-            <input name="release_year"value="{{ $series->release_year }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/series/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>End date</h3>
-            <input name="end_date"value="{{ $series->end_date }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/series/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Countries</h3>
-            <input name="countries"value="{{ $series->countries }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/series/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>PG Rating</h3>
-            <input name="pg_rating"value="{{ $series->pg_rating }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="PUT" action="/titles/series/{{$title->id}}">
-            <h3>Trailer URL</h3>
-            <input name="trailer"value="{{ $series->trailer }}">
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/series/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Genres:</h3>
-            <textarea name="genres">{{$genres}}</textarea>
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/series/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Creators:</h3>
-            <textarea name="creators">{{$creators}}</textarea>
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-          <form method="POST" action="/titles/series/{{$title->id}}">
-            {{ csrf_field() }}
-            {{ method_field('PUT') }}
-            <h3>Photos:</h3>
-            <textarea name="photos">{{$photos}}</textarea>
-            <button type="submit">Submit</button>   
-          </form>
-        </section>
-        <section>
-        </section>
+<article class="form-container">
+  <section>
+    <form method="POST" action="/titles/series/{{$title->id}}">
+      {{ csrf_field() }}
+      {{ method_field('PUT') }}
+      <h3>Title</h3>
+      <input class="input" name="title"value="{{ $series->title }}">
+      <button class="button is-info" type="submit">Submit</button>   
+    </form>
+  </section>
+  <section>
+    <form method="POST" action="/titles/series/{{$title->id}}">
+      {{ csrf_field() }}
+      {{ method_field('PUT') }}
+      <h3>Plot Summary</h3>
+      <textarea class="textarea" name="plot_summary">{{ $series->plot_summary }}"</textarea>
+      <button class="button is-info" type="submit">Submit</button>   
+    </form>
+  </section>
+  <section>
+    <form method="POST" action="/titles/series/{{$title->id}}">
+      {{ csrf_field() }}
+      {{ method_field('PUT') }}
+      <h3>Relesae Year</h3>
+      <input class="input" name="release_year"value="{{ $series->release_year }}">
+      <button class="button is-info" type="submit">Submit</button>   
+    </form>
+  </section>
+  <section>
+    <form method="POST" action="/titles/series/{{$title->id}}">
+      {{ csrf_field() }}
+      {{ method_field('PUT') }}
+      <h3>End date</h3>
+      <input class="input" name="end_date"value="{{ $series->end_date }}">
+      <button class="button is-info" type="submit">Submit</button>   
+    </form>
+  </section>
+  <section>
+    <form method="POST" action="/titles/series/{{$title->id}}">
+      {{ csrf_field() }}
+      {{ method_field('PUT') }}
+      <h3>Countries</h3>
+      <input class="input" name="countries"value="{{ $series->countries }}">
+      <button class="button is-info" type="submit">Submit</button>   
+    </form>
+  </section>
+  <section>
+    <form method="POST" action="/titles/series/{{$title->id}}">
+      {{ csrf_field() }}
+      {{ method_field('PUT') }}
+      <h3>PG Rating</h3>
+      <input class="input" name="pg_rating"value="{{ $series->pg_rating }}">
+      <button class="button is-info" type="submit">Submit</button>   
+    </form>
+  </section>
+  <section>
+    <form method="PUT" action="/titles/series/{{$title->id}}">
+      <h3>Trailer URL</h3>
+      <input class="input" name="trailer"value="{{ $series->trailer }}">
+      <button class="button is-info" type="submit">Submit</button>   
+    </form>
+  </section>
+  <section>
+    <form method="POST" action="/titles/series/{{$title->id}}">
+      {{ csrf_field() }}
+      {{ method_field('PUT') }}
+      <h3>Genres:</h3>
+      <textarea class="textarea" name="genres">{{$genres}}</textarea>
+      <button class="button is-info" type="submit">Submit</button>   
+    </form>
+  </section>
+  <section>
+    <form method="POST" action="/titles/series/{{$title->id}}">
+      {{ csrf_field() }}
+      {{ method_field('PUT') }}
+      <h3>Creators:</h3>
+      <textarea class="textarea" name="creators">{{$creators}}</textarea>
+      <button class="button is-info" type="submit">Submit</button>   
+    </form>
+  </section>
+  <section>
+    <form method="POST" action="/titles/series/{{$title->id}}">
+      {{ csrf_field() }}
+      {{ method_field('PUT') }}
+      <h3>Photos:</h3>
+      <textarea class="textarea" name="photos">{{$photos}}</textarea>
+      <button class="button is-info" type="submit">Submit</button>   
+    </form>
+  </section>
+  </article>
 @include('layouts.footer')
