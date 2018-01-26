c088b58bf38854c033245f772a6b90e2b411d28d
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-26 16:44:33 +0100

    remove unused pagination buttons

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -271,26 +271,6 @@
         @if($titles->hasMorePages())
         <a class="pagination-next" href="{{$titles->nextPageUrl()}}">Next page</a>
         @endif
-        <ul class="pagination-list">
-            <li>
-                <a class="pagination-link is-current" aria-label="Page 1" aria-current="page">1</a>
-            </li>
-            <li>
-                <a class="pagination-link" aria-label="Goto page 2">2</a>
-            </li>
-            <li>
-                <a class="pagination-link" aria-label="Goto page 3">3</a>
-            </li>
-            <li>
-                <a class="pagination-link" aria-label="Goto page 4">4</a>
-            </li>
-            <li>
-                <span class="pagination-ellipsis">&hellip;</span>
-            </li>
-            <li>
-                <a class="pagination-link" aria-label="Goto page 12">12</a>
-            </li>
-        </ul>
     </nav>
     @endif
 @include('layouts.footer')
