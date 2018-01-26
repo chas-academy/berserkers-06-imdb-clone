4ed9a6c718ee17b0de2140cca6faea5c2de33211
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-26 17:37:10 +0100

    add func for getting corect error messeges when resetting password and sending resetpasswordemail

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -37,4 +37,38 @@ class ResetPasswordController extends Controller
         $this->middleware('guest');
     }
 
+    protected function reset(Request $request)
+    { 
+        $this->validate($request, $this->rules(), $this->validationErrorMessages());
+        
+        // Here we will attempt to reset the user's password. If it is successful we
+        // will update the password on an actual user model and persist it to the
+        // database. Otherwise we will parse the error and return the response.
+        $response = $this->broker()->reset(
+            $this->credentials($request), function ($user, $password) {
+                $this->resetPassword($user, $password);
+            }
+        );
+
+        // If the password was successfully reset, we will redirect the user back to
+        // the application's home authenticated view. If there is an error we can
+        // redirect them back to where they came from with their error message.
+        return $response == Password::PASSWORD_RESET
+                    ? $this->sendResetResponse($request, $response)
+                    : $this->sendResetFailedResponse($request, $response);
+    }
+
+    protected function sendResetResponse($request , $response)
+    {   
+        $request->session()->flash('message', ['success' => 'Your password was changed!']);
+        return redirect($this->redirectPath());
+    }
+
+
+    protected function sendResetFailedResponse(Request $request, $response)
+    {
+        $request->session()->flash('message', ['error' => 'Your password could not be changed']);
+        return redirect()->back()->withInput($request->only('email'));
+    }
+
 }
