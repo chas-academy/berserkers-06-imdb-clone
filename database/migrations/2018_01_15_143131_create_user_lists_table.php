6171d7a53a3dcce5fd98e0880441a352f5284240
SebastianGerS <sebastian.gerstel.sollerman@chasacademy.se>
2018-01-26 17:55:17 +0100

    removed missused usecurrent() on migration

--- database/migrations/2018_01_15_143131_create_user_lists_table.php
+++ database/migrations/2018_01_15_143131_create_user_lists_table.php
@@ -17,7 +17,7 @@ class CreateUserListsTable extends Migration
             $table->increments('id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->string('name');
-            $table->timestamps()->useCurrent();
+            $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users');
         });
